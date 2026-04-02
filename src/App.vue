d360ac37da9d7202fe6abc683f6351efddcd7089
iVerdugoDev <ivan270@gmail.com>
2026-04-02 11:00:18 -0300

    crea ejemplo axios + interceptores

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { apiPlaceholder } from './api/apiPlaceholder'
+import { ref } from 'vue'
+
+const posts = ref([])
+const isLoading = ref(false)
+const error = ref('')
+
+// const fetchPosts = async () => {
+//   try {
+//     isLoading.value = true
+//     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+//     posts.value = response.data
+//   } catch (err) {
+//     console.error('Error en la petición', err)
+//     error.value = `Error en la petición: ${err}`
+//   } finally {
+//     isLoading.value = false
+//   }
+// }
+
+// const fetchPosts = async () => {
+//   try {
+//     isLoading.value = true
+//     const response = await apiPlaceholder.get('/posts')
+//     posts.value = response.data
+//   } catch (error) {
+//     error.value = `Error en la petición: ${err}`
+//   } finally {
+//     isLoading.value = false
+//   }
+// }
+
+const fetchPosts = async () => {
+  try {
+    const response = await apiPlaceholder.get('/posts')
+    posts.value = response
+  } catch (error) {
+    console.error('Error al obtener los posts', error)
+    error.value = `Error al obtener los posts ${error}`
+  }
+}
+
+// const fetchPhotos = async () => {
+//   try {
+//     isLoading.value = true
+//     const response = await apiPlaceholder.get('/photos')
+//     // posts.value = response.data
+//     // console.log(res)
+//   } catch (err) {
+//     error.value = `Error en la petición: ${err}`
+//   } finally {
+//     isLoading.value = false
+//   }
+// }
+
+fetchPosts()
+// fetchPhotos()
+</script>
+
+<template>
+  <main class="container">
+    <div class="spinner-border" role="status" v-if="isLoading">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <p class="text-danger lead" v-if="error">{{ error }}</p>
+    <ul class="list-group" v-if="!isLoading && !error">
+      <li class="list-group-item" v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p class="lead">{{ post.body }}</p>
+        <p class="small text-muted">Creado por: {{ post.userId }}</p>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/api/apiPlaceholder.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+
+export const apiPlaceholder = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: {
+    'Content-Type': 'application/json',
+    // si se pidiera JSON Web Token (JWT)
+    // 'Authorization': `Bearer ${localStorage.getItem('token')}`
+  },
+})
+
+apiPlaceholder.onLoadingChange = null
+
+// Interceptor de petición -> Request
+// Se ejecuta ante que la petición sea enviada al servidor
+apiPlaceholder.interceptors.request.use(
+  (config) => {
+    if (typeof apiPlaceholder.onLoadingChange === 'function') {
+      apiPlaceholder.onLoadingChange(true)
+    }
+    console.log('Interceptor de Request → Enviando petición a:', config.url)
+
+    return config
+  },
+  (error) => {
+    if (typeof apiPlaceholder.onLoadingChange === 'function') {
+      apiPlaceholder.onLoadingChange(false)
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+// Interceptor de respuesta -> Response
+apiPlaceholder.interceptors.response.use(
+  (response) => {
+    if (typeof apiPlaceholder.onLoadingChange === 'function') {
+      apiPlaceholder.onLoadingChange(false)
+    }
+
+    console.log('Respuesta recibida correctamente')
+    console.log('Response desde el interceptor', response)
+    return response.data // entregamos los datos desestructurados
+  },
+
+  (error) => {
+    if (typeof apiPlaceholder.onLoadingChange === 'function') {
+      apiPlaceholder.onLoadingChange(false)
+    }
+
+    if (error.response) {
+      console.error(`Error del servidor ${error.response.status}: ${error.response.data}`)
+
+      switch (error.response.status) {
+        case 401:
+          console.warn('Sesión expirada o no autorizada')
+          break
+        case 404:
+          console.warn('El recurso solicitado no fue encontrado')
+          break
+        case 500:
+          console.warn('Error interno del servidor, inténtelo de nuevo más tarde')
+          break
+      }
+    } else if (error.request) {
+      console.error('Error de red: No se pudo conectar con el servidor')
+    } else {
+      console.error('Error inesperado', error.message)
+    }
+
+    return Promise.reject(error)
+  }
+)
